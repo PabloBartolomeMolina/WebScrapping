a526330ce54e3bdbe65007170d3ac62cc3ae231c
Pablo Bartolomé <pablobart.me@gmail.com>
2023-05-23 23:10:21 +0200

    Another web, need to improve parameters to read table and extract it to csv

--- main.py
+++ main.py
@@ -5,28 +5,39 @@ import csv
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    URL = "http://www.values.com/inspirational-quotes"
+    URL = "https://racingnews365.com/every-world-champion-in-formula-1-history"
     r = requests.get(URL)
+    print(r.content)
 
     soup = BeautifulSoup(r.content, 'html5lib')
 
-    quotes = []  # a list to store quotes
+    data = []
+    table = soup.find('div', attrs={'class': 'panel__content'})
+    table_body = table.findAll('div', attrs={'class': 'qti-listm'})
 
-    table = soup.find('div', attrs={'id': 'all_quotes'})
-
-    for row in table.findAll('div',
-                             attrs={'class': 'col-6 col-lg-3 text-center margin-30px-bottom sm-margin-30px-top'}):
-        quote = {}
-        quote['theme'] = row.h5.text
-        quote['url'] = row.a['href']
-        quote['img'] = row.img['src']
-        quote['lines'] = row.img['alt'].split(" #")[0]
-        quote['author'] = row.img['alt'].split(" #")[1]
-        quotes.append(quote)
+    #rows = table_body.find_all('tr')
+    for row in table_body:
+        cols = row.find_all('td')
+        cols = [ele.text.strip() for ele in cols]
+        data.append([ele for ele in cols if ele])  # Get rid of empty values
 
     filename = 'inspirational_quotes.csv'
     with open(filename, 'w', newline='') as f:
-        w = csv.DictWriter(f, ['theme', 'url', 'img', 'lines', 'author'])
+        w = csv.DictWriter(f, ['Year', 'Driver'])
         w.writeheader()
-        for quote in quotes:
-            w.writerow(quote)
+        for quote in data:
+            w.writerow(data)
+'''
+    table = soup.find("table", { "class" : "lineItemsTable" })
+    for row in table.findAll("tr"):
+        cells = row.findAll("td")
+        if len(cells) == 9:
+            summons = cells[1].find(text=True)
+            plateType = cells[2].find(text=True)
+            vDate = cells[3].find(text=True)
+            location = cells[4].find(text=True)
+            borough = cells[5].find(text=True)
+            vCode = cells[6].find(text=True)
+            amount = cells[7].find(text=True)
+            print amount
+'''
