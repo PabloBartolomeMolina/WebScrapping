abebb5a4df46c38a2cb125b252ec6158b916292e
Pablo Bartolomé <pablobart.me@gmail.com>
2023-05-23 22:14:14 +0200

    Add some format to the printing using BeautifulSoup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-
+from bs4 import BeautifulSoup
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -7,5 +7,7 @@ if __name__ == '__main__':
     URL = "https://www.geeksforgeeks.org/data-structures/"
     r = requests.get(URL)
     print(r.content)
+    print("\n\n\n")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    soup = BeautifulSoup(r.content, 'html5lib')
+    print(soup.prettify())
