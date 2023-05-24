add6653c54b89ee7228f68776ad977d756629f7f
Pablo Bartolomé <pablobart.me@gmail.com>
2023-05-24 21:39:18 +0200

    Extraction of table with champions Print on console of Pycharm the talbe with format Clean up of code Commenting

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
-import csv
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
     URL = "https://racingnews365.com/every-world-champion-in-formula-1-history"
     r = requests.get(URL)
-    #print(r.content)
+    # print(r.content)
 
     soup = BeautifulSoup(r.content, 'html.parser')
-    results = soup.find(id="racingnews365_ros_alpha_leaderboard-billboard")
-    #print(results)
-    print(results.prettify())
+    # results = soup.find(id="racingnews365_ros_alpha_leaderboard-billboard") # get full web content
+    results = soup.find("table",
+                        {"class": "table-default table-default--expanded content-field__table"})  # get table contents
+    # print(results.prettify())  # print the content of extracted data
+
+    rows = results.find_all("tr")  # look for rows in the table with proper header identifier
+    # print("Champions:")
+    # print(champions)
+    for row in rows:
+        cells = row.findAll("td")  # look for each cell using proper identifier
+        if len(cells) == 2:
+            year = cells[0].find(text=True).replace(" ", "")
+            name = cells[1].find(text=True).replace(" ", "")
+            printable = year + " : " + name
+            print(year.replace("\n", "") + name.replace("\n", "") + "\n")
