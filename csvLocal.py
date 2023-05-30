bb3e771fbb072af305f8e9256494bbb2c131b5a4
Pablo Bartolomé <pablobart.me@gmail.com>
2023-05-30 21:44:08 +0200

    Minor reformat of the file

--- csvLocal.py
+++ csvLocal.py
@@ -1,6 +1,7 @@
 import csv
 
-def csvTable (champs_dict):
+
+def csvTable(champs_dict):
     with open('champions.csv', 'w') as f:
         for key in champs_dict.keys():
             f.write("%s,%s\n" % (key, champs_dict[key]))
