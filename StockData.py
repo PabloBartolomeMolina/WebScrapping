d781dbf2e811f947e0186dc18f92b743fd1c3dac
Pablo Bartolomé <pablobart.me@gmail.com>
2023-06-30 19:47:11 +0200

    Update function to get the stock data of a table in the web.

---
+++ StockData.py
@@ -0,0 +1,42 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import csv
+import matplotlib.pyplot as plt
+
+
+def get_stock_data(stock_url):
+    response = requests.get(stock_url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    dfs = pd.read_html(response.text)   # List with content of the table of data we look for.
+    print(type(dfs))
+
+    dfCount = 0
+    for df in dfs:
+        print(' DataFrame : ', dfCount)
+        print(df)
+        dfCount = dfCount + 1
+    print(dfs[5])
+
+    return dfs[5]
+
+
+def save_data_to_csv(stock_data, filename):
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['Timeframe', 'Stock Value'])
+
+        for timeframe, value in stock_data.items():
+            writer.writerow([timeframe, value])
+
+
+def plot_graph(stock_data):
+    timeframes = list(stock_data.keys())
+    values = list(stock_data.values())
+
+    plt.figure(figsize=(8, 6))
+    plt.bar(timeframes, values)
+    plt.xlabel('Timeframe')
+    plt.ylabel('Stock Value')
+    plt.title('Stock Evolution')
+    plt.show()
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-import scrapping
+import StockData
+from openpyxl import Workbook
+from openpyxl.utils.dataframe import dataframe_to_rows
 
-URL_example = "https://racingnews365.com/every-world-champion-in-formula-1-history"
-
+url_bourse = "https://www.boursorama.com/cours/1rPORA/"
 
 def main():
-    scrapping.webScrapping_Table(URL_example)
-
+    stock_data = StockData.get_stock_data(url_bourse)
+    print(type(stock_data))
+    wb = Workbook()
+    ws = wb.active
+    for r in dataframe_to_rows(stock_data, index=True, header=True):
+        ws.append(r)
+    wb.save("stockData.xlsx")
 
 if __name__ == "__main__":
     main()
