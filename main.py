3155d7020f97863cd0fc78a2e86a38c457ef6548
Pablo Bartolomé <pablobart.me@gmail.com>
2023-07-16 19:23:14 +0200

    CleanUp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from datetime import date
-from openpyxl.styles import Alignment, Font, PatternFill
+from openpyxl.styles import Alignment, Font
 from openpyxl import Workbook
 from openpyxl import load_workbook
 from openpyxl.utils.dataframe import dataframe_to_rows
@@ -9,8 +9,8 @@ import StockData
 url_bourse = "https://www.boursorama.com/cours/"
 excel_file = "D:/Python_Projects/WebScrapping/stockData.xlsx"
 stocks_dict = {
-    'Orange' : "1rPORA/",
-    'Alstom' : "1rPALO/"
+    'Orange': "1rPORA/",
+    'Alstom': "1rPALO/"
 }
 
 
@@ -25,11 +25,8 @@ def insert_dataframe(ws, start_row, start_col, df, index_col):
 
 
 def main():
-    today = date.today()
-
     if os.path.exists(excel_file):
         wb = load_workbook(excel_file)
-        ws = wb.worksheets[0]  # For the moment, unique worksheet existing for debugging purposes.
     else:
         wb = Workbook()
         for key in stocks_dict.keys():
@@ -38,22 +35,19 @@ def main():
         wb.save(excel_file)
 
     for key in stocks_dict.keys():
-        # stocks_dict['key']
         url_data = url_bourse + stocks_dict[key]
-        print(url_data)
         stock_data = StockData.get_stock_data(url_data)
-        print(type(stock_data))
-        print(stock_data)
 
         for ws in wb.worksheets:
-            if ws.title != key: continue
+            if ws.title != key:
+                continue
             else:
                 # Max cols in empty Excel file is 16384. Find the first cell empty and merge and fill it.
                 index_col = 0  # Used as index to shift data after first day recovering data.
                 for i in range(1, 16384, 4):
                     cell = ws.cell(1, i)
                     if cell.value is None:
-                        cell.value = today.strftime("%d-%m-%Y")
+                        cell.value = date.today().strftime("%d-%m-%Y")
                         ws.merge_cells(None, 1, i, 1, i + 3)
                         cell.alignment = Alignment(horizontal="center")
                         cell.font = Font(bold=True)
