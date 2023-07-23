be7e3db25f8b6f548b1bbe3a4cc3e397c685a049
Pablo Bartolomé <pablobart.me@gmail.com>
2023-07-23 16:11:59 +0200

    Reformat to reduce complexity and length of main function.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import os.path
 from datetime import date
-from openpyxl.styles import Alignment, Font
+
+import numpy
+import pandas
 from openpyxl import Workbook
 from openpyxl import load_workbook
+from openpyxl.styles import Alignment, Font
 from openpyxl.utils.dataframe import dataframe_to_rows
-import pandas, numpy
-import os.path
+
 import StockData
 import graphGeneration
 
@@ -24,6 +27,7 @@ year5VarList = []
 year10VarList = []
 
 
+# Read froml CSV into a dictionary.
 def readCSV(filename):
     data_dict = {}
     csv_data = pandas.read_csv(filename, sep=',', header=None)
@@ -44,66 +48,72 @@ def insert_dataframe(ws, start_row, start_col, df, index_col):
         cell.alignment = Alignment(horizontal='center')
 
 
+# Check if output file exists to load it or to create it.
+def outputFile_create(filename, stocks_dict):
+    if os.path.exists(filename):
+        wb = load_workbook(filename)
+        companies_list = list(stocks_dict.keys())
+        companies_list.sort()
+        for key in companies_list:
+            if key in wb.sheetnames:
+                print(key, " already present")
+                continue
+            else:
+                print(key, " newly added")
+                wb.create_sheet(key)  # Create a worksheet for newly added company.
+    else:
+        wb = Workbook()
+        for key in stocks_dict.keys():
+            wb.create_sheet(key)  # Create a worksheet per company.
+        del wb['Sheet']  # Remove worksheet created by default.
+        wb.save(filename)
+    return wb
+
+
 def main():
     if os.path.exists(companiesFile):
         stocks_dict = readCSV(companiesFile)
+        # Check if output file exists to load it or to create it.
+        wb = outputFile_create(excel_file, stocks_dict)
 
-        if os.path.exists(excel_file):
-            wb = load_workbook(excel_file)
-            companies_list = list(stocks_dict.keys())
-            companies_list.sort()
-            for key in companies_list:
-                if key in wb.sheetnames:
-                    print(key, " already present")
+        for key in stocks_dict.keys():
+            url_data = url_bourse + stocks_dict[key]
+            stock_data = StockData.get_stock_data(url_data)
+            stockList.append(key)
+            currentVarList.append(stock_data.values[0][1])
+            weekVarList.append(stock_data.values[1][1])
+            monthVarList.append(stock_data.values[2][1])
+            month3VarList.append(stock_data.values[3][1])
+            month6VarList.append(stock_data.values[4][1])
+            yearVarList.append(stock_data.values[5][1])
+            year3VarList.append(stock_data.values[6][1])
+            year5VarList.append(stock_data.values[7][1])
+            year10VarList.append(stock_data.values[8][1])
+
+            for ws in wb.worksheets:
+                if ws.title != key:
                     continue
                 else:
-                    print(key, " newly added")
-                    wb.create_sheet(key)  # Create a worksheet for newly added company.
-        else:
-            wb = Workbook()
-            for key in stocks_dict.keys():
-                wb.create_sheet(key)  # Create a worksheet per company.
-            del wb['Sheet']  # Remove worksheet created by default.
-            wb.save(excel_file)
+                    # Max cols in empty Excel file is 16384. Find the first cell empty and merge and fill it.
+                    index_col = 0  # Used as index to shift data after first day recovering data.
+                    for i in range(1, 16384, 4):
+                        cell = ws.cell(1, i)
+                        if cell.value is None:
+                            cell.value = date.today().strftime("%d-%m-%Y")
+                            ws.merge_cells(None, 1, i, 1, i + 3)
+                            cell.alignment = Alignment(horizontal="center")
+                            cell.font = Font(bold=True)
+                            index_col = index_col + 1
+                            break
+                        else:
+                            print(cell.value)  # Print date of already used cell.
+                    # Get data in the Excel file.
+                    insert_dataframe(ws, 2, 1, stock_data, index_col)
+                    wb.save(excel_file)
 
-            for key in stocks_dict.keys():
-                url_data = url_bourse + stocks_dict[key]
-                stock_data = StockData.get_stock_data(url_data)
-                stockList.append(key)
-                currentVarList.append(stock_data.values[0][1])
-                weekVarList.append(stock_data.values[1][1])
-                monthVarList.append(stock_data.values[2][1])
-                month3VarList.append(stock_data.values[3][1])
-                month6VarList.append(stock_data.values[4][1])
-                yearVarList.append(stock_data.values[5][1])
-                year3VarList.append(stock_data.values[6][1])
-                year5VarList.append(stock_data.values[7][1])
-                year10VarList.append(stock_data.values[8][1])
-
-                for ws in wb.worksheets:
-                    if ws.title != key:
-                        continue
-                    else:
-                        # Max cols in empty Excel file is 16384. Find the first cell empty and merge and fill it.
-                        index_col = 0  # Used as index to shift data after first day recovering data.
-                        for i in range(1, 16384, 4):
-                            cell = ws.cell(1, i)
-                            if cell.value is None:
-                                cell.value = date.today().strftime("%d-%m-%Y")
-                                ws.merge_cells(None, 1, i, 1, i + 3)
-                                cell.alignment = Alignment(horizontal="center")
-                                cell.font = Font(bold=True)
-                                index_col = index_col + 1
-                                break
-                            else:
-                                print(cell.value)  # Print date of already used cell.
-                        # Get data in the Excel file.
-                        insert_dataframe(ws, 2, 1, stock_data, index_col)
-                        wb.save(excel_file)
-
-                graphGeneration.plot_yearVariation(stockList, currentVarList, weekVarList, monthVarList, month3VarList,
-                                                   month6VarList, yearVarList, year3VarList, year5VarList,
-                                                   year10VarList)
+        graphGeneration.plot_yearVariation(stockList, currentVarList, weekVarList, monthVarList, month3VarList,
+                                           month6VarList, yearVarList, year3VarList, year5VarList,
+                                           year10VarList)
     else:
         print("No companies to get data from...")
         print("End of the program...")
