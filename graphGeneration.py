aa60651cd745bc6361d82648fa2150cda2cfae42
Pablo Bartolomé <pablobart.me@gmail.com>
2023-07-23 17:07:59 +0200

    Minor reformat of the file.

--- graphGeneration.py
+++ graphGeneration.py
@@ -30,10 +30,8 @@ def generate_graph(csvname):
 def plot_yearVariation(stocks, currentYearVar, weekVar, monthVar, month3Var, month6Var, yearVar, year3Var, year5Var, year10Var):
     index = 0
     for stock in stocks:
-        #fig, ax = plt.subplots()
         varts = ['CurrentYear', '1 week', '1 month', '3 months', '6 months', '1 year', '3 years', '5 years', '10 years']
-        counts = [currentYearVar[index], weekVar[index], monthVar[index], month3Var[index], month6Var[index],
-                  yearVar[index], year3Var[index], year5Var[index], year10Var[index]]
+        # Get values in format 'float' to properly show them in the plots.
         counts = [float(currentYearVar[index].strip("%")), float(weekVar[index].strip("%")), float(monthVar[index].strip("%")),
                   float(month3Var[index].strip("%")), float(month6Var[index].strip("%")), float(yearVar[index].strip("%")),
                   float(year3Var[index].strip("%")), float(year5Var[index].strip("%")), float(year10Var[index].strip("%"))]
@@ -41,7 +39,7 @@ def plot_yearVariation(stocks, currentYearVar, weekVar, monthVar, month3Var, mon
         plt.ylim(min(counts), max(counts))
         plt.bar(varts, counts)
         plt.ylabel('Variation')
-        plt.title('Variations stock ')
+        plt.title('Variations stock ', stock)
 
         plt.show()
 
